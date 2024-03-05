d312cb5a9f44807be5672b9861c433f6583eb886
mimi <112664286+mirellamedeiros@users.noreply.github.com>
2024-03-05 11:03:29 -0300

    Create temperature_converter.py
    WIP

---
+++ temperature_converter.py
@@ -0,0 +1,9 @@
+celsius_temp = "float(input("mimi, what's the temperature in celsius? "))
+
+
+def mimi_converter(c):
+  return (1.8 * c) + 32
+
+print("mimi, that's " + str(mimi_converter(celsius_temp)) + " in farenheit!")
+
+# note to self: expand it to convert to and from farenheit, celsius, and kelvin !
